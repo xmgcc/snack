e7d4377ada5db43681af70d88a68db55953c1138
ckelsel <ckelsel@hotmail.com>
2019-07-29 14:53:49 +0800

    显示一个节点

--- snack.c
+++ snack.c
@@ -1,129 +1,141 @@
-// 撞墙游戏结束
-//
 #include <curses.h>
-#include "linklist.h"
-// 全局变量用g_开头
-struct SnackLinkList *g_snack = NULL;
-// 蛇头的位置
-int g_head_x = 0;
-int g_head_y = 0;
-// 蛇走的方向
-int g_forward_x = 0;
-int g_forward_y = 0;
 
-// food
-int g_food_x = 0;
-int g_food_y = 0;
-int g_food_exist = 0;
 
 #define MAP_WIDTH 20
 #define MAP_HEIGHT 20
 
-void redraw_map();
-void create_snack();
-void destroy_snack();
 
-void game_over()
+struct Snack {
+    // x坐标
+    int x;
+    // y坐标
+    int y;
+};
+
+// 全局变量, g_前缀定义变量
+struct Snack g_snack;
+
+void init_map3()
 {
-    clear();
+    // 局部变量
+    int x;
+    int y;
 
-    printw("-------------------\n");
-    printw("       GAME OVER\n");
-    printw("-------------------\n");
+    // 打印行
+    for (x = 0; x < MAP_HEIGHT; x++)
+    {
+        // 打印列
+        for (y = 0; y < MAP_WIDTH; y++)
+        {
+            if(x == 0 || x == MAP_HEIGHT -1)
+            {
+                // 如果第一行或者最后一行 ,0, 19
+                printw("--");
+            }
+            else
+            {
+                // [1,18]
 
-    destroy_snack();
-    create_snack();
-
-    // 按任意键继续
-    getch();
-}
-
-void process_key()
-{
-    int key;
-    // man 3 timeout
-    timeout(1000);
-    key = getch();
-    switch (key) {
-        // 没有用户输入，按照当前方向移动
-        case ERR:
-            g_head_x += g_forward_x;
-            g_head_y += g_forward_y;
-            break;
-        case KEY_UP:
-            g_head_x--;
-
-            g_forward_x = -1;
-            g_forward_y = 0;
-            break;
-        case KEY_DOWN:
-            g_head_x++;
-
-            g_forward_x = 1;
-            g_forward_y = 0;
-            break;
-        case KEY_LEFT:
-            g_head_y--;
-
-            g_forward_x = 0;
-            g_forward_y = -1;
-            break;
-        case KEY_RIGHT:
-            g_head_y++;
-
-            g_forward_x = 0;
-            g_forward_y = 1;
-            break;
-        default:
-            // 打印其他按键
-            printw("   0%o, %c\n", key, key);
-            return;
-    }
-
-    if (g_head_x <= 0 || g_head_x >= MAP_WIDTH || g_head_y <= 0 ||
-        g_head_y >= MAP_HEIGHT) {
-        game_over();
-    } else {
-        linklist_insert(&g_snack, g_head_x, g_head_y);
-        linklist_delete_tail(g_snack);
+                if (y == 0 || y == MAP_WIDTH - 1)
+                {
+                    // 第1列或第19列
+                    if (y == 0)
+                    {
+                        printw("| ");
+                    }
+                    else
+                    {
+                        printw(" |");
+                    }
+                }
+                else
+                {
+                    // 2,3,4,5...18
+                    // 遇到蛇的节点就打印[]，否则打印空格
+                    if (x == g_snack.x && y == g_snack.y)
+                    {
+                        printw("[]");
+                    }
+                    else
+                    {
+                        printw("  ");
+                    }
+                }
+            }
+        }
+        printw("\n");
     }
 }
 
-void init_ncurse()
-{
-    // 不显示输入的按键
-    noecho();
 
-    // Special keys. In order to capture special keystrokes like Backspace,
-    // Delete and the four arrow keys by getch(), you need to call
-    keypad(stdscr, TRUE);
-}
-
-void redraw_map()
+void init_map2()
 {
     int x;
     int y;
 
-    // 清屏,光标复位
-    clear();
-
-    for (x = 0; x < MAP_WIDTH; x++) {
-        for (y = 0; y < MAP_HEIGHT; y++) {
-            if (x == 0 || x == MAP_WIDTH - 1) {
-                printw("--");
-            } else if (y == 0 || y == MAP_HEIGHT - 1) {
-                if (y == 0) {
-                    printw("| ");
-                } else {
-                    printw(" |");
+    for (x = 0; x < MAP_HEIGHT; x++)
+    {
+        for (y = 0; y < MAP_WIDTH; y++)
+        {
+            if (x == 0 || x == 19)
+            {
+                // red
+                printw("##");
+            }
+            else 
+            {
+                // green
+                if (y == 0 || y == 19)
+                {
+                    if (y == 0)
+                    {
+                        printw("# ");
+                    }
+                    else
+                    {
+                        printw(" #");
+                    }
                 }
-            } else {
-                // 打印蛇的身子或者空格
-                if (linklist_find(g_snack, x, y) == 0) {
-                    printw("[]");
-                } else if (g_food_x == x && g_food_y == y) {
+                else
+                {
+                    printw("  ");
+                }
+            }
+        }
+        printw("\n");
+    }
+}
+    
+void init_map()
+{
+    int x;
+    int y;
+    // print
+    for (x = 0; x < MAP_WIDTH; x++)
+    {
+        for (y = 0; y < MAP_HEIGHT; y++)
+        {
+            // 0 || 19
+            if (y == 0 || y == MAP_HEIGHT - 1)
+            {
+                if (y == 0)
+                {
                     printw("# ");
-                } else {
+                }
+                else
+                {
+                    printw(" #");
+                }
+            }
+            else
+            {
+                // 0 || 19
+                if (x == 0 || x == MAP_WIDTH -1)
+                {
+                    printw("##");
+                }
+                else
+                {
                     printw("  ");
                 }
             }
@@ -132,57 +144,20 @@ void redraw_map()
     }
 }
 
-void create_snack()
-{
-    g_snack = linklist_create(1, 1, 3);
-    g_head_x = 1;
-    g_head_y = 3;
-
-    // 默认向右移动
-    g_forward_y = 1;
-}
-
-void destroy_snack()
-{
-    while (linklist_delete_tail(g_snack) == 0) {
-    }
-}
-
-void food()
-{
-    // 创建food
-    if (!g_food_exist) {
-        g_food_exist = 1;
-        g_food_x = rand() % MAP_WIDTH;
-        g_food_y = rand() % MAP_HEIGHT;
-    }
-
-    // 吃到food
-    if (g_head_x == g_food_x && g_head_y == g_food_y) {
-        g_food_exist = 0;
-        linklist_insert(&g_snack, g_food_x, g_food_y);
-    }
-}
-
 int main()
 {
-    WINDOW *win = initscr();
-    nodelay(win, TRUE);
+    // init
+    initscr();
 
-    init_ncurse();
+    g_snack.x = 3;
+    g_snack.y = 3;
+    
+    init_map3();
 
-    create_snack();
-
-    redraw_map();
-
-    while (1) {
-        process_key();
-
-        food();
-        redraw_map();
-    }
-
+    // wait key
+    getch();
+    
+    // destroy
     endwin();
-
     return 0;
 }
